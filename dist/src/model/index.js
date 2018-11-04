faf3739b1f36cf8de586098939d0316f25774c81
emaxwell14 <emaxwell1414@hotmail.com>
2018-11-04 11:44:23 +0000

    adding babel

--- dist/src/model/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var User = require('./User');
-
-module.exports = {
-    User: User
-};
--- dist/src/service/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var configService = require('./configService');
-var userService = require('./userService');
-
-module.exports = {
-    configService: configService,
-    userService: userService
-};
